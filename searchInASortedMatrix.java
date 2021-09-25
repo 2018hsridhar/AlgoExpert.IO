2da432e25d85b1f0c385f8741694e79802752af2
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-09-25 15:52:24 -0400

    Create searchInASortedMatrix.java

---
+++ searchInASortedMatrix.java
@@ -0,0 +1,39 @@
+/*
+
+Brute Force Solution : 
+Let M := number of rows in the matrix
+Let N := number of cols in the matrix
+
+Evaluate each element and perform element-by-element comparison
+if target value is in the matrix OR not
+If in matrix, return that pair
+Otherwise, return {-1,-1}
+
+Complexity : 
+RunTime = O(MN) [Polynomial - Quadratic ]
+Space = O(1)
+
+Denoting a column matrix as if it were typed as a row matrix, via {} closing brackets
+Edge Case Testing : 
+(A) [1] target = 1, target = 0
+(B) [1,3,6,8,9]  targets = {-1,1,3,5,9,10} : search within bounds is a good way!
+(C) {}
+(D) [2,3
+		6,7]		Square Matrix
+(E) [2,3,6
+		6,7,10]		Rectangular Matrix
+(F)
+(G)
+
+
+*/
+import java.util.*
+
+Rectangular Matri(F)
+	(G)
+	
+  public static int[] searchInSortedMatrix(int[][] matrix, int target) {
+    // Write your code here.
+    return new int[] {-1, -1};
+  }
+}
