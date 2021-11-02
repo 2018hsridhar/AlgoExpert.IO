1b1903e3be4854f9fb1df436c3195120a6e7c0f7
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-11-01 21:49:25 -0400

    Create shortenPath.java

---
+++ shortenPath.java
@@ -0,0 +1,38 @@
+/*
+Function takes in a non-empty string representing a valid UNIX-shell path
+Return the SHORTERED/ABRIDGED version of said path
+Paths may be absolute paths or relative paths 
+
+/ := the directory seperator
+.. := the parent directory
+. := the current directory
+Once at root directory, future parent directory accesses perform no operation as well
+/ ../../..
+
+> Common pattern : long->short path, and both point to the same shared element
+Abriged path and long path must point to the same file
+
+COMPLEXITY
+Let N := length of path
+Let D := the depth of thy path
+Time = 
+Space = 
+
+TEST BENCH
+(A)
+(B)
+(C)
+(D)
+
+
+*/
+import java.util.*;
+
+class Program 
+{
+  public static String shortenPath(String path) 
+	{
+    // Write your code here;
+    return "";
+  }
+}
