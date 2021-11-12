feaeae4d7e3bf214cc91058952628a2b511ada69
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-11-11 21:29:14 -0500

    Create subarraySort.java

---
+++ subarraySort.java
@@ -0,0 +1,36 @@
+/*												 
+													 (        ) : len = 3 here
+													 
+Maybe O(N) storage of the encountered (min,max) also needed as well?
+Dynamic data structures to incorporate
+
+ 0	1		2		3		4		5		6		7		8		9		10	11	12
+[1,	2,	4,	7,	10,	11,	7,	12,	6,	7,	16,	18,	19]
+ -----------------------^ 
+ ---------  <=======================>  ----------
+
+Strategies : Nested Intervals, Recursion, DP, Two Pointers ( left,right meet-in-the-middle ) 
+Can also grab the global minima and the global maxima
+Can also consider the sliding window technique as well
+
+Subarray to sort : NOT to reverse
+
+TEST BENCH : 
+(A)
+(B)
+(C)
+(D)
+(E)
+
+*/
+
+import java.util.*;
+
+class Program 
+{
+  public static int[] subarraySort(int[] array) 
+	{
+    // Write your code here.
+    return new int[] {};
+  }
+}
