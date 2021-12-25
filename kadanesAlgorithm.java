3cb419139cff77cbce8e91dbf0b49c3b7e2fbd72
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-12-25 16:31:03 -0500

    Update kadanesAlgorithm.java

--- kadanesAlgorithm.java
+++ kadanesAlgorithm.java
@@ -1,47 +1,65 @@
-/*
+Given a non empty array of integers
+Return the max sum obtainable by summing up all integers in a non-empty subarray of the input array
 
-THOUGHT PROCESS : 
+array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+
+19 // [1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1]
+
+PSEUDOCODE : 
+
+	maxSum = MIN_VALUE;
+	runSum = 0
+	for each element in the array
+			runSum += elem
+			maxSum = max(maxSum, runSum)
+			if(runSum <= 0)
+				runSum = 0
+	return maxSum
 
 COMPLEXITY
-Let N := len(array)
-Time = 
-Space =
+Let N := # elements in the input array
+TIME = O(N)
+SPACE = O(1)
+Greedy algorithm : solves for smaller subproblems at each step
 
-TEST BENCH
-(A) [1,2,3,4,5]
-(B) [-1,-2,-3,-4,-5]
-(C) [-1,-2,-3,-4,0]
-(D) [1,2,3,-2,5,6,-7,8]
-(E) [1,2,4,-8,1]
-(F) [1,2,4,-8,1,2,100]
-(G) [1,2,4,-5,1,2,10,-10,20]
+Can have duplicate input
 
-*/
+TEST CASES
+Array always non-empty
+(A) [1,4,7,11]
+	=> take entire sum up to 23
+(B) [1,4,7,-1]
+	=> 12
+(C) [1,2,5,9,12,-1]	
+	Negative as the last : but all else is good here
+(D) [1,4,7,-2,1000]
+	1+4+7 = 12 - 2 = 10 + 1000	( have a dip : not zeroed out here or negative )
+(E) [1,4,7,-100,1000]
+	1+4+7 = 12 - 100 = -88 ( +1000 ) : brings us too low ( have a dip : massive decrement )
+(F) [1,2,3,-6,5,7]
+		 --------
+		 0
+		 1+2+3+-6+5+7 = 5 + 7 = 12
+(G) [1,2,3,-6,0,0,5,7]
+(H) [-6,-4,-7,-1,-2]
+	-1 ( subarray at idx = 3 )
+(I) [1]
+(J) [-1]
+
 import java.util.*;
 
-class Program 
-{
+class Program {
   public static int kadanesAlgorithm(int[] array) 
 	{
-		if(array == null || array.length == 0)
-			return 0;
-		
 		int maxSum = Integer.MIN_VALUE;
-		int runningSum = 0;
-		int n = array.length;
-		for(int i = 0; i < n; ++i)
+		int runSum = 0;
+		for(int i = 0; i < array.length; ++i)
 		{
-			runningSum += array[i];
-			// Single standalone element test anyways ( seems taken care of by initial running sum )?
-			if(array[i] > maxSum)
-				maxSum = array[i];
-			if(runningSum > maxSum)
-				maxSum = runningSum;
-			if(runningSum < 0)
-				runningSum = 0;
-		}
-		
-		
+			runSum += array[i];
+			maxSum = Math.max(maxSum, runSum);
+			runSum = Math.max(0, runSum);
+		}			
 		return maxSum;
   }
 }
+
