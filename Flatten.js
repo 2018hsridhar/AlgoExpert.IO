c68e249a2e0e71e657b87f3b8883e39a428d48a9
Hari Sridhar <hari.h.sridhar@gmail.com>
2023-12-30 18:02:07 -0800

    Create Flatten.js
    Writing equivalent of Array.prototype.flatten in JS

---
+++ Flatten.js
@@ -0,0 +1,25 @@
+/*
+Input : A random value
+Output : A flattened version of said value
+
+1. Primitives left unchanged
+2. Nested arrs -> vals to topLevel arr
+3. Nested Objects -> key removal
+  key collision : use ANY associated value ( just leave as-is )
+4. 
+5.
+
+No value is a function :-)
+Object key type. Here, all strings.
+
+*/
+function flatten(value) {
+
+
+
+
+
+}
+
+// Do not edit the line below.
+exports.flatten = flatten;
